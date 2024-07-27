f0c986eb9660e0ce04592b14adafeef01e4fbdf2
ShaldonS <eeemeeny@gmail.com>
2024-07-28 00:20:02 +0300

    add dist

--- README.md
+++
@@ -1,2 +0,0 @@
-# InventoryVue
-Inventory app written with Vue 3 composition api, SCSS, Pinia, TypeScript
--- inventory-app/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import { createPinia } from 'pinia'
-
-createApp(App).use(createPinia()).mount('#app')
